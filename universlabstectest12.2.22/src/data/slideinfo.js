ca18ee6c8e220f44e7abd6f7a5fb8df43bce034d
Georgia <ghobs987@gmail.com>
2022-02-13 17:07:31 +0000

    data to template

--- universlabstectest12.2.22/src/data/slideinfo.js
+++ universlabstectest12.2.22/src/data/slideinfo.js
@@ -1,1 +1,71 @@
-export const slideObjArr = [];
+export const slideObjArr = [
+  {
+    heading: "See the Big Picture, Act on the Small Details",
+    cta: {
+      label: "CONTACT US",
+      url: "#",
+    },
+    slides: {
+      title: "Carousel 1",
+      excerpt: "this is a para",
+      backgroundImage: `${process.env.PUBLIC_URL}/assets/images/finn.jpg`,
+      cta: {
+        label: "READ NOW",
+        url: "#",
+      },
+    },
+  },
+  {
+    heading: "Slide 2 header",
+    cta: {
+      label: "cta label",
+      url: "#",
+    },
+    slides: {
+      title: "Carousel 2",
+      excerpt: "this is a para2",
+      backgroundImage: `${process.env.PUBLIC_URL}/assets/images/harley.jpg`,
+      cta: {
+        label: "READ NOW 2",
+        url: "#",
+      },
+    },
+  },
+];
+
+export const websiteInformation = {
+  heading: "stuff",
+  cta: {
+    label: "stuff",
+    url: "stuff",
+  },
+  slides: [
+    {
+      title: "Carousel 1",
+      excerpt: "this is a para2",
+      backgroundImage: `${process.env.PUBLIC_URL}/assets/images/harley.jpg`,
+      cta: {
+        label: "READ NOW 2",
+        url: "#",
+      },
+    },
+    {
+      title: "Carousel 2",
+      excerpt: "this is a para2",
+      backgroundImage: `${process.env.PUBLIC_URL}/assets/images/harley.jpg`,
+      cta: {
+        label: "READ NOW 2",
+        url: "#",
+      },
+    },
+    {
+      title: "Carousel 2",
+      excerpt: "this is a para2",
+      backgroundImage: `${process.env.PUBLIC_URL}/assets/images/harley.jpg`,
+      cta: {
+        label: "READ NOW 2",
+        url: "#",
+      },
+    },
+  ],
+};
