5a1636186831f3eb8c5b4479eac2df5c8a009ca3
Georgia <ghobs987@gmail.com>
2022-02-14 23:07:07 +0000

    scroll btn

--- universlabstectest12.2.22/src/App.js
+++ universlabstectest12.2.22/src/App.js
@@ -1,14 +1,16 @@
-
 import { Slides } from "./components/slides/Slides";
 import "./App.css";
-
+import { CgArrowLongDown } from "react-icons/cg";
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Slides />
-      
-      <div className="vector37"></div>
+
+      <div className="hLine"></div>
+      <div className="">
+        <CgArrowLongDown className="arrow" />
+      </div>
       <p className="scrollformore">SCROLL FOR MORE</p>
       <div className="vector35"></div>
     </div>
